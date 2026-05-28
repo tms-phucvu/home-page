fd90f9b086726a8c397a83ba90f358451155ea91
tms-phucvu <phuc.vu@tomosia.com>
2026-05-28 18:15:49 +0700

    fix: remove  middleware

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from "next-intl/middleware"
-import { routing } from "./src/i18n/routing"
-
-export default createMiddleware(routing)
-
-export const config = {
-  // Match only internationalized pathnames
-  matcher: ["/", "/(ja|en)/:path*"],
-}
